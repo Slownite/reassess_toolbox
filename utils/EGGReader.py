33ff7ba27d905a014fe20be1dea72612937f9981
BoutainaELYAZIJI <boutaina.mumu11@gmail.com>
2024-04-03 16:01:21 +0200

    new file:   test/TestAudio.py 	modified:   utils/EGGReader.py 	test/output_audio.aac

--- utils/EGGReader.py
+++ utils/EGGReader.py
@@ -82,3 +82,58 @@ class EEGReaderMNE:
             int: Total number of samples.
         """
         return self.total_samples
+
+
+class EEGStream:
+    def __init__(self, edf_path, chunk_duration=1.0):
+        """
+        Initialize an EEGStreamMNE object for streaming EEG data in chunks.
+
+        Args:
+            edf_path (str): Path to the EDF file.
+            chunk_duration (float): Duration of each chunk in seconds.
+        """
+        self.edf_path = edf_path
+        self.chunk_duration = chunk_duration
+        self.raw = mne.io.read_raw_edf(edf_path, preload=False)
+        self.sampling_freq = self.raw.info['sfreq']
+        self.num_channels = len(self.raw.ch_names)
+        self.total_samples = self.raw.n_times
+        self.annotations = self.raw.annotations
+        self.duration = self.total_samples / self.sampling_freq
+
+    def stream(self):
+        """
+        Stream EEG data in chunks.
+
+        Yields:
+            np.ndarray: The next chunk of EEG data.
+        """
+        start_time = 0
+        while start_time < self.duration:
+            end_time = start_time + self.chunk_duration
+            if end_time > self.duration:
+                end_time = self.duration
+
+            # Fetching the chunk of data
+            start_sample = int(start_time * self.sampling_freq)
+            end_sample = int(end_time * self.sampling_freq)
+            data_chunk = self.raw.get_data(start=start_sample, stop=end_sample)
+
+            yield data_chunk
+
+            start_time += self.chunk_duration
+
+    def get_annotations_in_chunk(self, chunk_start, chunk_end):
+        """
+        Get annotations that fall within a specific chunk of data.
+
+        Args:
+            chunk_start (float): Start time of the chunk in seconds.
+            chunk_end (float): End time of the chunk in seconds.
+
+        Returns:
+            list: List of annotations within the specified chunk.
+        """
+        annotations_in_chunk = [annot for annot in self.annotations if chunk_start <= annot['onset'] < chunk_end]
+        return annotations_in_chunk
