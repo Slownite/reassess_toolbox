0187c31c0c65b503e2529cc4f828b2d5b8c93757
Slownite <snfdiop@outlook.com>
2024-04-17 16:41:23 +0200

    start dataset

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,4 @@
 from .stream import VideoStreamer
+from .AudioStream import AudioStream, AudioEager
+from .EGGReader import EEGReader, EEGStream
+from .EGGVideoSynchronizer import EEGVideoSynchronizer
