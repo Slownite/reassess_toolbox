22b2fdc769a06d5de8e031268af3e10bed222d7b
BoutainaELYAZIJI <boutaina.mumu11@gmail.com>
2024-04-30 14:56:18 +0200

    modified AudioStream.py

--- utils/AudioStream.py
+++ utils/AudioStream.py
@@ -1,6 +1,12 @@
 import librosa
 import numpy as np
 import soundfile as sf 
+import moviepy.editor as mpy
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.stream import VideoStreamer
 
 class AudioEager:
     def __init__(self, filepath, sr=None):
@@ -63,6 +69,128 @@ class AudioEager:
 
         """
         return self.total_samples / self.sr
+    
+# class AudioStream:
+#     def __init__(self, filepath, chunk_duration=1.0, sr=None):
+#             """
+#             Initialize an AudioStream object to handle streaming audio data in chunks from a file.
+
+#             Args:
+#                 filepath (str): Path to the audio file.
+#                 chunk_duration (float): Duration of each chunk in seconds.
+#                 sr (int, optional): Sampling rate to use for loading the audio. If None, use the file's original sample rate.
+#             """
+#             self.filepath = filepath
+#             self.chunk_duration = chunk_duration
+#             self.sr = sr
+
+#         # Access the metadata of the audio file to get sample rate and total samples
+#             with sf.SoundFile(filepath) as f:
+#                 self.sr = sr if sr is not None else f.samplerate
+#                 self.total_samples = f.frames
+#                 self.total_duration = self.total_samples / self.sr
+
+#     # def __getitem__(self, index):
+#     #     """
+#     #     Get the audio data for a specific index from the video stream.
+
+#     #     Args:
+#     #         index (int): The index of the video chunk from which to extract audio.
+
+#     #     Returns:
+#     #         np.ndarray: The audio data extracted from the video chunk.
+#     #     """
+#     #     # Retrieve the video chunk using the VideoStreamer
+#     #     video_clip = mpy.VideoFileClip(self.video_streamer.video_paths[index])  # Assuming the video path can be accessed directly
+#     #     audio_clip = video_clip.audio
+#     #     audio_data = audio_clip.to_soundarray(fps=44100)  # Extract audio as an array
+#     #     video_clip.close()
+
+#     def stream(self):
+#         """
+#         Stream audio file in blocks.
+
+#         Yields:
+#             np.ndarray: The next block of audio samples.
+#         """
+#         # Use librosa.stream with the specified frame_length and hop_length
+#         return librosa.stream(self.filepath, 
+#                               block_length=self.batch_size, 
+#                               frame_length=self.frame_length, 
+#                               hop_length=self.hop_length)
+
+#     def get_samples(self, start_sample=0, end_sample=None):
+#         """
+#         Get a specific range of samples from the audio file in a streaming context.
+
+#         Args:
+#             start_sample (int): Starting index for samples to retrieve.
+#             end_sample (int, optional): Ending index for samples to retrieve. If None, all samples until the end of the file are retrieved.
+
+#         Returns:
+#             np.ndarray: The requested range of audio samples.
+#         """
+#         collected_samples = []
+#         current_sample = 0
+
+#         for block in self.stream():
+#             block_start_sample = current_sample
+#             block_end_sample = current_sample + len(block)
+#             current_sample += len(block)
+
+#             # Skip blocks before the start_sample
+#             if block_end_sample <= start_sample:
+#                 continue
+
+#             # If the current block contains the start_samp                      le
+#             if start_sample < block_end_sample:
+#                 start_index = max(0, start_sample - block_start_sample)
+#                 end_index = None if end_sample is None else end_sample - block_start_sample
+
+#                 # If the current block reaches or exceeds the end_sample
+#                 if end_index is not None and end_index <= len(block):
+#                     collected_samples.append(block[start_index:end_index])
+#                     break
+#                 else:
+#                     collected_samples.append(block[start_index:])
+
+#             # Stop if we have reached the end_sample
+#             if end_sample is not None and current_sample >= end_sample:
+#                 break
+
+#         # Concatenate collected samples into a single array to return
+#         return np.concatenate(collected_samples, axis=0) if collected_samples else np.array([])
+    
+
+#     def get_samples_with_soundfile(self, start_sample=0, end_sample=None):
+
+#             with sf.SoundFile(self.filepath, 'r') as f:
+#                 # Seek to the start sample
+#                 f.seek(start_sample)
+#                 # Calculate the number of samples to read
+#                 num_samples = end_sample - start_sample if end_sample else self.total_samples - start_sample
+#                 # Read and return the specified range of samples
+#                 samples = f.read(num_samples)
+#                 return samples
+    
+#     def __len__(self):
+#         """
+#         Get the total number of audio samples.
+
+#         Returns:
+#             int: The total number of audio samples.
+#         """
+#         return self.total_samples
+    
+#     def get_duration(self):
+#         """
+#         Get the duration of the audio file in seconds.
+
+#         Returns:
+#             float: The duration of the audio file in seconds.
+#         """
+#         return self.total_samples / self.sr
+
 
 class AudioStream:
     def __init__(self, filepath, batch_size=1024, sr=None, frame_length=2048, hop_length=1024):
@@ -86,7 +214,7 @@ class AudioStream:
             self.sr = sr if sr is not None else f.samplerate
             self.total_samples = len(f)
 
-    def stream(self):
+    def __getitem__(self):
         """
         Stream audio file in blocks.
 
