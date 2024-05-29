396e4dceb82a74a40fdb648e705047493b20ad8c
Samuel Diop <snfdiop@outlook.com>
2024-05-29 12:08:57 +0200

    add tqdm support

--- scripts/I3D_extractor.py
+++ scripts/I3D_extractor.py
@@ -107,7 +107,7 @@ def extract_and_save(
     model.to(device)
     model.eval()
     with torch.no_grad():
-        for data in loader:
+        for data in tqdm(loader, total=len(loader.dataset) // batch_size):
             data = data.to(device, non_blocking=True)
             embeddings = model(data)
             write_embedding_to_file_in_chunks(embeddings.cpu(), filename)
