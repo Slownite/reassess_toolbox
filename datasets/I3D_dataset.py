d43eaff0c9926f8283380ef7838716c85514c0a2
Samuel Diop <snfdiop@outlook.com>
2025-01-30 15:44:18 +0100

    debug information

--- datasets/I3D_dataset.py
+++ datasets/I3D_dataset.py
@@ -211,7 +211,7 @@ def debug_multi_edf_npy():
     dataset = MultiNpyEdf(args.npy, args.edf, args.schema)
     for i in range(len(dataset)):
         input = dataset[i]
-        assert isinstance(input, tuple) is True
+        assert input.sahep == 2
         print(
             f"label: {input[1]} at position start {i * 64} end {(i * 64) + 64}")
 
