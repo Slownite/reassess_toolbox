4c1947b56e7fd2737765e6943fb704922f9b2f23
Samuel Diop <snfdiop@outlook.com>
2025-01-30 17:04:36 +0100

    transformer training script

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,1 +1,1 @@
-from .I3D_dataset import I3D_embeddings, MultiNpyEdf
+from .I3D_dataset import I3D_embeddings, MultiNpyEdf, MultiNpyEdfSequence
---
+++ train/base_transformer.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+import pathlib
+import logging
+import hydra
+from omegaconf import DictConfig
+from torch.utils.data import DataLoader, Dataset, random_split
+from torch import nn
+from torch.optim import Adam
+from torch.optim.lr_scheduler import StepLR
+import torch
+from datasets import MultiNpyEdfSequence
+from utils import save_model_weights, save_loss, downsample, write_dict_to_csv
+from modules import ProjectedTransformer
+from sklearn.metrics import (
+    confusion_matrix,
+    precision_score,
+    recall_score,
+    f1_score,
+    accuracy_score,
+    roc_auc_score,
+    fbeta_score
+)
+import numpy as np
+
+logging.basicConfig(level=logging.INFO)
+
+
+def load_dataset(
+    dataset_path: pathlib.Path,
+    schema_json: pathlib.Path,
+    model: str,
+    sequence_length: int = 10,
+    b_size=5,
+    shuffle=True,
+    n_workers=2,
+) -> DataLoader:
+    """
+    Load the dataset using MultiNpyEdfSequence.
+    """
+    npy_files = dataset_path.rglob("0rgb_*x3d*.npy")
+    edf_files = dataset_path.rglob("*.edf")
+
+    dataset = MultiNpyEdfSequence(
+        npy_files, edf_files, schema_json, sequence_length=sequence_length)
+    return dataset
+
+
+def split_dataset(dataset: Dataset, test_split: float, seed: int = 0):
+    """
+    Split the dataset into training and testing sets.
+    """
+    test_size = int(len(dataset) * test_split)
+    train_size = len(dataset) - test_size
+    train_data, test_data = random_split(
+        dataset, [train_size, test_size], generator=torch.manual_seed(seed))
+    return train_data, test_data
+
+
+def init(cfg: DictConfig) -> tuple[nn.Module, DataLoader, DataLoader]:
+    """
+    Initialize the model and data loaders.
+    """
+    model = ProjectedTransformer(
+        input_dim=cfg.transformer.input_dim,
+        d_model=cfg.transformer.d_model,
+        nhead=cfg.transformer.nhead,
+        num_layers=cfg.transformer.num_layers,
+        num_classes=cfg.target
+    )
+    dataset = load_dataset(
+        cfg.data_path,
+        cfg.schema_path,
+        cfg.model,
+        sequence_length=cfg.sequence_length
+    )
+
+    train_data, test_data = split_dataset(
+        dataset, cfg.test_split, seed=cfg.seed)
+    train_loader = DataLoader(train_data, batch_size=cfg.batch_size,
+                              shuffle=cfg.shuffle, num_workers=2, pin_memory=True)
+    test_loader = DataLoader(test_data, batch_size=cfg.batch_size,
+                             shuffle=False, num_workers=2, pin_memory=True)
+
+    return model, train_loader, test_loader
+
+
+def train(
+    cfg: DictConfig,
+    device: torch.device,
+    n_epochs: int = 1,
+) -> nn.Module:
+    """
+    Train the model.
+    """
+    model, train_loader, _ = init(cfg)
+    optimizer = Adam(model.parameters(), lr=cfg.learning_rate)
+    scheduler = StepLR(
+        optimizer, step_size=cfg.stepLR.step_size, gamma=cfg.stepLR.gamma)
+    loss_fn = nn.BCEWithLogitsLoss()
+    model = model.to(device)
+    model.train()
+
+    for epoch in range(n_epochs):
+        logging.info(f"Starting epoch {epoch + 1}/{n_epochs}.")
+        epoch_loss = 0
+        num_batches = len(train_loader)
+
+        for batch_number, sequences in enumerate(train_loader):
+            try:
+                X = torch.stack([seq[0] for seq in sequences])
+                y = torch.stack([seq[1] for seq in sequences])
+                optimizer.zero_grad()
+                X, y = X.to(device), y.to(device).float()
+                y_pred = model(X)
+                loss = loss_fn(y_pred.squeeze(), y)
+                loss.backward()
+                optimizer.step()
+
+                epoch_loss += loss.item()
+                save_loss(loss.item(), cfg.path_to_model_save /
+                          f"loss_epoch{epoch+1}_batch{batch_number+1}.txt")
+            except Exception as e:
+                logging.error(f"Error in batch {batch_number + 1}: {e}")
+
+        avg_epoch_loss = epoch_loss / num_batches
+        logging.info(
+            f"Epoch {epoch + 1} completed. Average Loss: {avg_epoch_loss:.4f}")
+        scheduler.step()
+    return model
+
+
+@hydra.main(config_path="conf/X3D/", config_name="transformer.yaml")
+def main(cfg: DictConfig) -> None:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = train(cfg, device=device, n_epochs=cfg.epochs)
+    save_model_weights(model, cfg.path_to_model_save /
+                       f"{cfg.model}_lr{cfg.learning_rate}_epochs{cfg.epochs}.pth")
+
+
+if __name__ == "__main__":
+    main()
